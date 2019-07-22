5682d916cff585b1090a080fb61327e0fd82960a
dhruv1999 <dhruv.anita100@gmail.com>
2019-07-22 23:43:50 +0530

    client id changed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {};
-   config.oAuthClientID = '1090389463761-9o4mrakp79i48npnhmd92inh2lalul61';
-   config.oAuthclientSecret = 'oZuCLxtGrORgzhXv-ieDpMUa';
+   config.oAuthClientID = '247502799799-nfjuirk469n065m0jipl22akfdq6nhht.apps.googleusercontent.com';
+   config.oAuthclientSecret = '1iEAUkuUX3NXd4ZwdCd02uKo';
    config.oAuthCallbackUrl = 'https://shielded-brushlands-68752.herokuapp.com/auth/google/callback';
    config.port = process.env.PORT || 8080;
    config.scopes = [
@@ -12,3 +12,7 @@ const config = {};
     config.albumPageSize = 50;
     config.apiEndpoint = 'https://photoslibrary.googleapis.com';
 module.exports = config;
+
+
+
+//{"web":{"client_id":"247502799799-nfjuirk469n065m0jipl22akfdq6nhht.apps.googleusercontent.com","project_id":"photo-247517","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"1iEAUkuUX3NXd4ZwdCd02uKo"}}
